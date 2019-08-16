c0652fb350976785dbbd6a6dac8054528088c693
TanguyH <tanguyh@icloud.com>
2019-08-16 18:14:05 +0200

    Hydra launchers

---
+++ Reimbursement/hydra/launchers/re_hydra_peer_TT.py
@@ -0,0 +1,17 @@
+import sys
+import numpy as np
+import reimbursement
+
+if __name__ == "__main__":
+    # mu, sigma case analysis
+    mu = np.float64(sys.argv[1])
+    s = np.float64(sys.argv[2])
+
+    # N case analysis
+    #s = np.float64(sys.argv[1])
+    #N = int(sys.argv[2])
+
+    #case = sys.argv[3]
+
+    model = reimbursement.SocialLearningSim(N = 5, M = 100, c = 1., r = 3., G = .7, B = .7, gamma = .7, beta = .7, sigma = 1., alpha = .7, mu = mu, s = s, compulsory = True, SOP = True, case="peer")
+    model.runAnalyticalSim()
